e8c2dd73a299e9cfc08879b2b97cf5ff34109066
Пользователь <cocosi383@gmail.com>
2022-12-31 14:53:16 +0300

    pep8

--- app/services/constants.py
+++ app/services/constants.py
@@ -3,15 +3,15 @@ from datetime import datetime
 
 FORMAT = "%Y/%m/%d %H:%M:%S"
 NOW_DATE_TIME = datetime.now().strftime(FORMAT)
-SPREADSHEETS_BODY = { 
-        'properties': {'title': f'QRKot_Отчет на {NOW_DATE_TIME}', 
-                       'locale': 'ru_RU'}, 
-        'sheets': [{ 
-            'properties': {'sheetType': 'GRID', 
-                           'sheetId': 0, 
-                           'title': 'Скорость закрытия', 
-                           'gridProperties': {'rowCount': 100, 
-                                              'columnCount': 11} 
-                           } 
-        }] 
+SPREADSHEETS_BODY = {
+        'properties': {'title': f'QRKot_Отчет на {NOW_DATE_TIME}',
+                       'locale': 'ru_RU'},
+        'sheets': [{
+            'properties': {'sheetType': 'GRID',
+                           'sheetId': 0,
+                           'title': 'Скорость закрытия',
+                           'gridProperties': {'rowCount': 100,
+                                              'columnCount': 11}
+                           }
+        }]
     }
